3c0e3931505845e930a3fc820a4f0815ebd7251c
xamdp <64017084+xamdp@users.noreply.github.com>
2025-02-02 19:31:17 +0800

    Add code experiment
    Added some random code from the internet, I will be basing here some of
    my works in the main branch.
    
    The key differences from our code is that, I tried to create the all the
    divs based on the user input, but the `flex-basis` is not changing.
    
    Which resulted in some weird sized divs that are not equally matching
    the desired outcome.

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 
+
 const genBtn = document.getElementById('genBtn');
 genBtn.addEventListener('click', function() {
 	const input = prompt('Enter a number of box per side:')
-	generateGridBox(input);
+	changeNumberOfGridSquares(input);
 });
 
-/*generate the grix-box*/
-function generateGridBox(boxes) {
-	const totalBoxes = (boxes * boxes); 
-	const containerHasChild = document.querySelector('.container').hasChildNodes();
-	if (containerHasChild) {
-		const parent =  document.querySelector('.container');
-		parent.replaceChildren();
-		createBox(totalBoxes);
-	} else {
-		createBox(totalBoxes);
+function createGridColumn(num) {
+	for (let j = 0; j < num; j++) {
+		createGridRow(num, j);
 	}
 }
 
-/*creates the actual boxes*/
-function createBox (totalBoxes) {
-	const container = document.querySelector('.container');
-	for (let i = 0; i < totalBoxes; i++) {
-		const div = document.createElement('div');
-		div.classList.add('box', 'color-box')
-		container.appendChild(div);
+function createGridRow(num, index) {
+	const row = document.createElement('div');
+	row.classList.add('row');
+	row.classList.add('row-' + index)
+	document.getElementById('grid-container').appendChild(row);
+	for (let i = 0; i < num; i++) {
+		let div = document.createElement('div')
+		div.id = 'div-' + uniqueIdentifier;
+		// div.style.background = "red";
+		div.style.borderStyle = "solid";
+		div.style.borderColor = "black";
+		div.style.borderWidth = "1px";
+		div.onmouseover =  (div) => {
+			document.getElementById(div.toElement.id).style.background = "red";
+		};
+		document.querySelector(`.row-${index}`).appendChild(div);
+		uniqueIdentifier++;
 	}
-
-	const boxes = document.querySelectorAll('.color-box')
-	boxes.forEach((box) => {
-		box.addEventListener("mouseover", () => {
-			const currentColor = box.style.backgroundColor;
-			if (currentColor === '') {
-				box.style.backgroundColor = 'pink';
-			} else if (currentColor  === 'pink'){
-				box.style.backgroundColor = 'red';
-			} else if (currentColor === 'red') {
-				box.style.backgroundColor = 'pink';
-			}
-		})
-	})
-
-	boxes.forEach((box) => {
-		box.addEventListener("mouseout", () => {
-			const currentColor = box.style.backgroundColor;
-			if (currentColor === 'red') {
-				box.style.backgroundColor = 'red';
-			} else if (currentColor  === 'red'){
-				box.style.backgroundColor = 'pink';
-			}
-		})
-	})
 }
+
+function changeNumberOfGridSquares(input) {
+	const maxGridSquares = 100;
+	// numberOfGridSquares = prompt("Enter number boxes per side");
+	let numberOfGridSquares =  input;
+	if (numberOfGridSquares > maxGridSquares) {
+		console.log("limit reached");
+		numberOfGridSquares = maxGridSquares;
+	}
+	
+	let grid = document.getElementById("grid-container");
+	grid.innerHTML = "";
+	createGridColumn(numberOfGridSquares);
+}
+
+let numberOfGridSquares =  16;
+let uniqueIdentifier = 0;
+let gridSquaresButton = document.getElementById('square-grid-button');
+// gridSquaresButton.addEventListener("click", changeNumberOfGridSquares);
+createGridColumn(16);
+
