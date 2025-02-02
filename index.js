39f81f6ae52324ec484348bc66e3e11420731849
xamdp <64017084+xamdp@users.noreply.github.com>
2025-02-02 11:22:57 +0800

    Refactor the process of creating the boxes
    Based from my last commit, I've managed to create a `16x16` grid box but the button is not implemented yet.
    
    As of now, the generate button is working as intended, whenever a user wants to generate a new canvas, it prompts the user, asking
    for the boxes per side. Then it checks if a grid-boxes are already existing, if it is, it clears it and generate a new one based on the input.
    
    As for the event listeners, I've temporarily moved them inside a `createBox()`. I had a hard time making the `mouseover` and
    `mouseout` event listeners work. As the boxes are created, somehow the listeners are already listening for a non-existing class which is the
    `.color-box`. This is based only my assumption, which is why I tried to move the event listeners right after the boxes are created.
    
    I noticed that, by doing that, event listeners we're able to recognize the `.color-box` class created after clicking the generate button.
    
    But this approach is ugly for me, I want my `createBox()` not to be chunky in code.
    
    I was thinking that maybe I can toggle the css class for `.color-box` when the event listeners are triggered.

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 
-const container = document.querySelector('.container');
-
-for (let i = 0; i < 256; i++) {
-	const div = document.createElement('div');
-	div.classList.add('box', 'color-box')
-	container.appendChild(div);
-}
-
-const boxes = document.querySelectorAll('.color-box');
-
-
-boxes.forEach(box => {
-	box.addEventListener("mouseover", () => {
-		const currentColor = box.style.backgroundColor;
-		if (currentColor === '') {
-			box.style.backgroundColor = 'pink';
-		} else if (currentColor  === 'pink'){
-			box.style.backgroundColor = 'red';
-		} else if (currentColor === 'red') {
-			box.style.backgroundColor = 'pink';
-		}
-	});
+const genBtn = document.getElementById('genBtn');
+genBtn.addEventListener('click', function() {
+	const input = prompt('Enter a number of box per side:')
+	generateGridBox(input);
 });
 
-boxes.forEach(box => {
-	box.addEventListener("mouseout", function() {
-		const currentColor = box.style.backgroundColor;
-		if (currentColor === 'red') {
-			box.style.backgroundColor = 'red';
-		} else if (currentColor === 'red'){
-			box.style.backgroundColor = 'pink';
-		}
-	});
-})
+/*generate the grix-box*/
+function generateGridBox(boxes) {
+	const totalBoxes = (boxes * boxes); 
+	const containerHasChild = document.querySelector('.container').hasChildNodes();
+	if (containerHasChild) {
+		const parent =  document.querySelector('.container');
+		parent.replaceChildren();
+		createBox(totalBoxes);
+	} else {
+		createBox(totalBoxes);
+	}
+}
 
+/*creates the actual boxes*/
+function createBox (totalBoxes) {
+	const container = document.querySelector('.container');
+	for (let i = 0; i < totalBoxes; i++) {
+		const div = document.createElement('div');
+		div.classList.add('box', 'color-box')
+		container.appendChild(div);
+	}
+
+	const boxes = document.querySelectorAll('.color-box')
+	boxes.forEach((box) => {
+		box.addEventListener("mouseover", () => {
+			const currentColor = box.style.backgroundColor;
+			if (currentColor === '') {
+				box.style.backgroundColor = 'pink';
+			} else if (currentColor  === 'pink'){
+				box.style.backgroundColor = 'red';
+			} else if (currentColor === 'red') {
+				box.style.backgroundColor = 'pink';
+			}
+		})
+	})
+
+	boxes.forEach((box) => {
+		box.addEventListener("mouseout", () => {
+			const currentColor = box.style.backgroundColor;
+			if (currentColor === 'red') {
+				box.style.backgroundColor = 'red';
+			} else if (currentColor  === 'red'){
+				box.style.backgroundColor = 'pink';
+			}
+		})
+	})
+}
